737ccb9cf6cd102a41c7af2ca7799488de38f731
Felix Abecassis <fabecassis@nvidia.com>
2017-05-23 16:13:50 -0400

    Add automated GitLab CI build and push
    The images are built on a x86_64 runner using binfmt_misc and QEMU

--- push.sh
+++ push.sh
@@ -7,9 +7,15 @@ OS="ubuntu16.04"
 NO_OS_SUFFIX="true"
 LATEST="true"
 
+if [ $(uname -m) = "ppc64le" ]; then
+    QEMU_PPC64LE=""
+else
+    QEMU_PPC64LE="-v /usr/bin/qemu-ppc64le-static:/usr/bin/qemu-ppc64le-static"
+fi
+
 VERSION="8.0"
-docker build --pull -t "${REPOSITORY}:${VERSION}-runtime-${OS}" "${VERSION}/runtime"
-docker build -t "${REPOSITORY}:${VERSION}-devel-${OS}" "${VERSION}/devel"
+docker build --pull ${QEMU_PPC64LE} -t "${REPOSITORY}:${VERSION}-runtime-${OS}" "${VERSION}/runtime"
+docker build ${QEMU_PPC64LE} -t  "${REPOSITORY}:${VERSION}-devel-${OS}" "${VERSION}/devel"
 if [[ "${NO_OS_SUFFIX}" == true ]]; then
     docker tag "${REPOSITORY}:${VERSION}-runtime-${OS}" "${REPOSITORY}:${VERSION}-runtime";
     docker tag "${REPOSITORY}:${VERSION}-devel-${OS}" "${REPOSITORY}:${VERSION}-devel";
@@ -21,8 +27,8 @@ fi
 VERSION="8.0-cudnn5"
 CUDA_VERSION="8.0"
 CUDNN_VERSION="cudnn5"
-docker build -t "${REPOSITORY}:${VERSION}-runtime-${OS}" "${CUDA_VERSION}/runtime/${CUDNN_VERSION}"
-docker build -t "${REPOSITORY}:${VERSION}-devel-${OS}" "${CUDA_VERSION}/devel/${CUDNN_VERSION}"
+docker build ${QEMU_PPC64LE} -t "${REPOSITORY}:${VERSION}-runtime-${OS}" "${CUDA_VERSION}/runtime/${CUDNN_VERSION}"
+docker build ${QEMU_PPC64LE} -t "${REPOSITORY}:${VERSION}-devel-${OS}" "${CUDA_VERSION}/devel/${CUDNN_VERSION}"
 if [[ "${NO_OS_SUFFIX}" == true ]]; then
     docker tag "${REPOSITORY}:${VERSION}-runtime-${OS}" "${REPOSITORY}:${VERSION}-runtime";
     docker tag "${REPOSITORY}:${VERSION}-devel-${OS}" "${REPOSITORY}:${VERSION}-devel";
@@ -31,8 +37,8 @@ fi
 VERSION="8.0-cudnn6"
 CUDA_VERSION="8.0"
 CUDNN_VERSION="cudnn6"
-docker build -t "${REPOSITORY}:${VERSION}-runtime-${OS}" "${CUDA_VERSION}/runtime/${CUDNN_VERSION}"
-docker build -t "${REPOSITORY}:${VERSION}-devel-${OS}" "${CUDA_VERSION}/devel/${CUDNN_VERSION}"
+docker build ${QEMU_PPC64LE} -t "${REPOSITORY}:${VERSION}-runtime-${OS}" "${CUDA_VERSION}/runtime/${CUDNN_VERSION}"
+docker build ${QEMU_PPC64LE} -t "${REPOSITORY}:${VERSION}-devel-${OS}" "${CUDA_VERSION}/devel/${CUDNN_VERSION}"
 if [[ "${NO_OS_SUFFIX}" == true ]]; then
     docker tag "${REPOSITORY}:${VERSION}-runtime-${OS}" "${REPOSITORY}:${VERSION}-runtime";
     docker tag "${REPOSITORY}:${VERSION}-devel-${OS}" "${REPOSITORY}:${VERSION}-devel";
