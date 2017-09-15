dd7b58ee6731aa80b87cc7b1d057525edde06214
Felix Abecassis <fabecassis@nvidia.com>
2017-09-14 23:49:52 -0400

    Add cuDNN v7

--- push.sh
+++ push.sh
@@ -44,4 +44,14 @@ if [[ "${NO_OS_SUFFIX}" == true ]]; then
     docker tag "${REPOSITORY}:${VERSION}-devel-${OS}" "${REPOSITORY}:${VERSION}-devel";
 fi
 
+VERSION="8.0-cudnn7"
+CUDA_VERSION="8.0"
+CUDNN_VERSION="cudnn7"
+docker build ${QEMU_PPC64LE} -t "${REPOSITORY}:${VERSION}-runtime-${OS}" "${CUDA_VERSION}/runtime/${CUDNN_VERSION}"
+docker build ${QEMU_PPC64LE} -t "${REPOSITORY}:${VERSION}-devel-${OS}" "${CUDA_VERSION}/devel/${CUDNN_VERSION}"
+if [[ "${NO_OS_SUFFIX}" == true ]]; then
+    docker tag "${REPOSITORY}:${VERSION}-runtime-${OS}" "${REPOSITORY}:${VERSION}-runtime";
+    docker tag "${REPOSITORY}:${VERSION}-devel-${OS}" "${REPOSITORY}:${VERSION}-devel";
+fi
+
 docker push "${REPOSITORY}"
